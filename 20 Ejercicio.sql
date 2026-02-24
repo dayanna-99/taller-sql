5b2f283de1d505107a1b94544386178fe5565c26
Dayanna-99 <dayana.oc1999@gmail.com>
2026-02-24 18:50:45 -0500

    Solucion vigésimo ejercicio

---
+++ 20 Ejercicio.sql
@@ -0,0 +1,14 @@
+-- Ejercicio 20
+-- Ingreso total, unidades vendidas y ticket promedio por categoría
+
+SELECT 
+    p.categoria,
+    SUM(v.total_venta) AS ingreso_total,
+    SUM(d.cantidad) AS total_unidades,
+    AVG(v.total_venta) AS ticket_promedio
+FROM detalle_ventas d
+JOIN productos p 
+    ON d.id_producto = p.id_producto
+JOIN ventas v 
+    ON d.id_venta = v.id_venta
+GROUP BY p.categoria;
