200899dc9cbcb7e4d6d280c366f0dec8fd8f8f6d
Dayanna-99 <dayana.oc1999@gmail.com>
2026-02-24 18:45:18 -0500

    Solucion dieciochoavo ejercicio

---
+++ 18 Ejercicio.sql
@@ -0,0 +1,12 @@
+-- Clientes mayores de 60 con total gastado
+
+SELECT 
+    c.nombre,
+    (CAST(strftime('%Y','now') AS INTEGER) 
+     - CAST(strftime('%Y', c.fecha_nacimiento) AS INTEGER)) AS edad_aproximada,
+    SUM(v.total_venta) AS total_gastado
+FROM clientes c
+JOIN ventas v 
+    ON c.id_cliente = v.id_cliente
+GROUP BY c.id_cliente
+HAVING edad_aproximada > 60;
